57263e6816b4536e493d124193ba0dbccfb7c11a
Nunthapop Nganiam <nuntha_pop@hotmail.com>
2023-12-17 07:19:59 +0700

    Create admin class and make some edit

--- database.py
+++ database.py
@@ -1,21 +1,94 @@
 # try wrapping the code below that reads a persons.csv file in a class and make it more general such that it can read in any csv file
 
-import csv, os
+import csv, os, copy
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-persons = []
-with open(os.path.join(__location__, 'persons.csv')) as f:
-    rows = csv.DictReader(f)
-    for r in rows:
-        persons.append(dict(r))
-print(persons)
+class Read:
+    def read_data(self, file_name):
+        data = []
+        with open(os.path.join(__location__, file_name)) as f:
+            rows = csv.DictReader(f)
+            for r in rows:
+                data.append(dict(r))
+        return data
 
 # add in code for a Database class
+class Database:
+    def __init__(self):
+        self.database = []
+
+    def insert(self, table):
+        self.database.append(table)
+
+    def search(self, table_name):
+        for table in self.database:
+            if table.table_name == table_name:
+                return table
+        return None
+
 
 # add in code for a Table class
+class Table:
+    def __init__(self, table_name, table):
+        self.table_name = table_name
+        self.table = table
 
+    def join(self, other_table, common_key):
+        joined_table = Table(self.table_name + '_joins_' + other_table.table_name, [])
+        for item1 in self.table:
+            for item2 in other_table.table:
+                if item1[common_key] == item2[common_key]:
+                    dict1 = copy.deepcopy(item1)
+                    dict2 = copy.deepcopy(item2)
+                    dict1.update(dict2)
+                    joined_table.table.append(dict1)
+        return joined_table
+
+    def insert(self, inserted_table):
+        self.table.append(inserted_table)
+
+    def update(self,user_id,change,updated):
+        for i in self.table:
+            print(i)
+
+    def filter(self, condition):
+        filtered_table = Table(self.table_name + '_filtered', [])
+        for item1 in self.table:
+            if condition(item1):
+                filtered_table.table.append(item1)
+        return filtered_table
+
+    def __is_float(self, element):
+        if element is None:
+            return False
+        try:
+            float(element)
+            return True
+        except ValueError:
+            return False
+
+    def aggregate(self, function, aggregation_key):
+        temps = []
+        for item1 in self.table:
+            if self.__is_float(item1[aggregation_key]):
+                temps.append(float(item1[aggregation_key]))
+            else:
+                temps.append(item1[aggregation_key])
+        return function(temps)
+
+    def select(self, attributes_list):
+        temps = []
+        for item1 in self.table:
+            dict_temp = {}
+            for key in item1:
+                if key in attributes_list:
+                    dict_temp[key] = item1[key]
+            temps.append(dict_temp)
+        return temps
+
+    def __str__(self):
+        return self.table_name + ':' + str(self.table)
 # modify the code in the Table class so that it supports the insert operation where an entry can be added to a list of dictionary
 
 # modify the code in the Table class so that it supports the update operation where an entry's value associated with a key can be updated
--- project_manage.py
+++ project_manage.py
@@ -1,40 +1,117 @@
 # import database module
-
+from database import Read, Table, Database
+from random import randint
 # define a funcion called initializing
+my_DB = Database()
+read = Read()
+
+class Admin:
+    def __init__(self, id, username, role, person_table, login_table):
+        self.id = id
+        self.username = username
+        self.role = role
+        self.person_table = person_table
+        self.login_table = login_table
+
+
+def manage_info(self):
+        while True:
+            user_choice = input('Select an option:\n '
+                                '1.Add an information\n '
+                                '2.Delete an information\n '
+                                '3.Edit an information \n'
+                                '4.Go back\n')
+            if user_choice == '1':
+                user_id = input('User_id: ')
+                first_name = input('First name: ')
+                last_name = input('Last name: ')
+                type = input('Type: ')
+                username = f'{first_name}.{last_name[0]}'
+                password = randint(1000,9999)
+                self.person_table.table.append({'ID': user_id, 'first': first_name, 'last': last_name, 'type': type})
+                self.login_table.table.append({'ID': user_id, 'username': username, 'password': password, 'role': type})
+                print(f'{user_id} {username} has added to the database')
+            elif user_choice == '2':
+                remove_id = input('User_id you want to delete: ')
+                for person in self.person_table.table:
+                    if person['ID'] == remove_id:
+                        self.person_table.table.remove(person)
+                for person in self.login_table.table:
+                    if person['ID'] == remove_id:
+                        self.login_table.table.remove(person)
+            elif user_choice == '3':
+                edit_id = input('User_id you want to edit: ')
+                for person in self.login_table.table:
+                    if person['ID'] == edit_id:
+                        print(f"{person['ID']}\n"
+                              f"{person['username']} {person['password']}\n"
+                              f" {person['role']}")
+                change_choice = input('Select what to change:\n'
+                                      '1.Password\n2.Role')
+                if change_choice == '1':
+                    new_password = randint(1000,9999)
+                    for person in self.login.table.table:
+                        if person['ID'] == edit_id:
+                            person['password'] = new_password
+                    print('The new password is:',new_password)
+                elif change_choice == '2':
+                    new_role = input('Enter new role:')
+                    for person in self.login_table.table:
+                        if person['ID'] == edit_id:
+                            person['role'] = new_role
+            elif user_choice == '4':
+                break
+            else:
+                print('Choice invalid. Please try again!')
+
 
 def initializing():
-    pass
-
-# here are things to do in this function:
-
+    # here are things to do in this function:
     # create an object to read all csv files that will serve as a persistent state for this program
-
+    read_person = read.read_data('persons.csv')
+    read_login = read.read_data('login.csv')
+    read_project = read.read_data('project.csv')
     # create all the corresponding tables for those csv files
-
+    person_table = Table('persons', read_person)
+    login_table = Table('login', read_login)
+    project_table = Table('project',read_project)
     # see the guide how many tables are needed
-
     # add all these tables to the database
+    my_DB.insert(person_table)
+    my_DB.insert(login_table)
+    my_DB.insert(project_table)
+
 
 
 # define a funcion called login
 
 def login():
-    pass
+    while True:
+        username = input('Enter username: ')
+        password = input('Enter password: ')
+        user_info = my_DB.search('login')
+        for i in user_info.table:
+            if username == i['username'] and password == i['password']:
+                return i['ID'], i['role']
+            else:
+                print('Invalid username or password. Please try again.')
+                return None
 
 # here are things to do in this function:
-   # add code that performs a login task
-        # ask a user for a username and password
-        # returns [ID, role] if valid, otherwise returning None
+# add code that performs a login task
+# ask a user for a username and password
+# returns [ID, role] if valid, otherwise returning None
 
 # define a function called exit
 def exit():
     pass
 
+
 # here are things to do in this function:
-   # write out all the tables that have been modified to the corresponding csv files
-   # By now, you know how to read in a csv file and transform it into a list of dictionaries. For this project, you also need to know how to do the reverse, i.e., writing out to a csv file given a list of dictionaries. See the link below for a tutorial on how to do this:
-   
-   # https://www.pythonforbeginners.com/basics/list-of-dictionaries-to-csv-in-python
+# write out all the tables that have been modified to the corresponding csv files
+# By now, you know how to read in a csv file and transform it into a list of dictionaries. For this project, you also need to know how to do the reverse, i.e., writing out to a csv file given a list of dictionaries. See the link below for a tutorial on how to do this:
+
+# https://www.pythonforbeginners.com/basics/list-of-dictionaries-to-csv-in-python
 
 
 # make calls to the initializing and login functions defined above
@@ -45,17 +122,17 @@ val = login()
 # based on the return value for login, activate the code that performs activities according to the role defined for that person_id
 
 # if val[1] = 'admin':
-    # see and do admin related activities
+# see and do admin related activities
 # elif val[1] = 'student':
-    # see and do student related activities
+# see and do student related activities
 # elif val[1] = 'member':
-    # see and do member related activities
+# see and do member related activities
 # elif val[1] = 'lead':
-    # see and do lead related activities
+# see and do lead related activities
 # elif val[1] = 'faculty':
-    # see and do faculty related activities
+# see and do faculty related activities
 # elif val[1] = 'advisor':
-    # see and do advisor related activities
+# see and do advisor related activities
 
 # once everyhthing is done, make a call to the exit function
 exit()
